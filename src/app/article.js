3be9e127f29b4fe7af7a5238b336ef0e89c53179
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-01-25 14:01:13 -0300

    updated 'About' and 'Contact Me' text

--- src/app/article.js
+++ src/app/article.js
@@ -15,10 +15,7 @@ export function initializeArticle() {
 export function setAboutArticle() {
   const articleContent = {
     title: 'About me',
-    excerpt: `"To make a complete guide on all the Pokémon in the world… 
-    That was my dream! But, I'm too old! I can't do it! So, I want you to 
-    fulfill my dream for me! Get moving! This is a great undertaking in 
-    Pokémon history!"  <strong>-- JP Bernard</strong>`,
+    excerpt: `This simple project is an exercise of the introductory course to JavaScript provided by <a href="https://argentinaprograma.com" target="_blank">"r/Argentina Programa"</a>. The data used for this exercise was provided by <a href="https://pokeapi.co/" target="_blank">PokeAPI.co</a> API. The web design and page layout is based on the design of the PokeAPI.co webpage.`,
   };
 
   renderArticle(articleContent);
@@ -27,8 +24,7 @@ export function setAboutArticle() {
 export function setContactArticle() {
   const articleContent = {
     title: 'Contact me',
-    excerpt: `"To make a complete guide on all the Pokémon in the world… 
-    That was my dream!"  <strong>-- JP Bernard</strong>`,
+    excerpt: `Greets! My name is <strong>Juan Pablo Bernard</strong>, a Web Dev in the making, and you can find more about me on <a href="https://www.linkedin.com/in/juan-pablo-bernard/" target="_blank">LinkeIn</a> or <a href="https://github.com/bernardjp" target="_blank">GitHub</a>.`,
   };
 
   renderArticle(articleContent);
